7a1e2562aec332ef6af1b1983aadfece5d3c631d
Bastiaan van de Kamer <bas@duchambre.media>
2020-12-03 18:18:42 +0100

    added dist

--- wp-futter.php
+++ wp-futter.php
@@ -2,7 +2,7 @@
 /**
  * Plugin Name: WP Futter (Instagram feed)
  * Description: Instagram feed for WP created in VueJS
- * Version: 1.09
+ * Version: 1.11
  * License: GPL2+
  * Author: Growise <bas@growise.pro>
  * Author URI: https://growise.pro
